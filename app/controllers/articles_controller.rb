5af21b78633b5dcb9ad34ce63d4437b627e971c3
Keri Spencer <keri.spencer@redvector.com>
2018-01-17 16:54:47 -0500

    Complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:edit, :udpate, :show, :destroy]
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -19,8 +21,6 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
-
-  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def show
   end
@@ -52,5 +52,12 @@ class ArticlesController < ApplicationController
 
     def set_article
       @article = Article.find(params[:id])
+    end
+
+    def require_same_user
+      if current_user != @article.user
+        flash[:danger] = "You can only edit or delete your own article"
+        redirect_to root_path
+      end
     end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,7 @@
 class PagesController < ApplicationController
-  def about
-
-  end
-
   def homepage
-
+    redirect_to articles_path if logged_in?
+  end
+  def about
   end
 end
